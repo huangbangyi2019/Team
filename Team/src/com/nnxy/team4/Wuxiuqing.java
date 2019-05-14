10f15295206fcf9d90d586f3d1522d65af64476b
huangbangyi2019 <1094476088@qq.com>
2019-05-14 17:43:29 +0800

    记录本次实验

---
+++ Team/src/com/nnxy/team4/Wuxiuqing.java
@@ -0,0 +1,41 @@
+package com.nnxy.team4;
+
+public class Wuxiuqing {
+	 private int sex;
+	 private int id;
+	 private int age;
+	 private String name;
+	 public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+	 
+
+}

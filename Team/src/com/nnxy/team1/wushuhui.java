f69f55037c397ffbec8f79059f1d4f23c75dedb7
huangbangyi2019 <1094476088@qq.com>
2019-05-14 17:48:10 +0800

    你真可爱

---
+++ Team/src/com/nnxy/team1/wushuhui.java
@@ -0,0 +1,41 @@
+package com.nnxy.team1;
+
+public class wushuhui {
+	private int id;
+	private String name;
+	private int password;
+	private String sex;
+	private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+}

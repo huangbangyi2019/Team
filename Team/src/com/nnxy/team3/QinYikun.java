13a1077e5c8f036d86c18044016a718b22f377bd
853924164 <853924164@qq.com>
2019-05-14 19:05:34 +0800

    充满七万的一天

---
+++ Team/src/com/nnxy/team3/QinYikun.java
@@ -0,0 +1,35 @@
+package com.nnxy.team3;
+
+public class QinYikun {
+
+	private int id;
+	private int cds;
+	private int ef;
+	private int asdc;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getCds() {
+		return cds;
+	}
+	public void setCds(int cds) {
+		this.cds = cds;
+	}
+	public int getEf() {
+		return ef;
+	}
+	public void setEf(int ef) {
+		this.ef = ef;
+	}
+	public int getAsdc() {
+		return asdc;
+	}
+	public void setAsdc(int asdc) {
+		this.asdc = asdc;
+	}
+	
+	
+}

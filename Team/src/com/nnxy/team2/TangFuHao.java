3f969281f56f896153364ca9f12db96d40c49993
huangbangyi2019 <48992563+huangbangyi2019@users.noreply.github.com>
2019-05-21 17:39:09 +0800

    Update TangFuHao.java

--- Team/src/com/nnxy/team2/TangFuHao.java
+++ Team/src/com/nnxy/team2/TangFuHao.java
@@ -6,15 +6,33 @@ public class TangFuHao {
 	private int age;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		setInformation(062,"ÌÆ¸»ºÀ",18);
+		setInformation(062,"ÃŒÃ†Â¸Â»ÂºÃ€",18);
 		
 		
 	}
 
 	private static void setInformation(int id,String name,int age) {
 		// TODO Auto-generated method stub
-		System.out.println("ID:"+id+"ĞÕÃû£º"+name+"ÄêÁä£º"+age);
+		System.out.println("ID:"+id+"ÃÃ•ÃƒÃ»Â£Âº"+name+"Ã„ÃªÃÃ¤Â£Âº"+age);
 		
 	}
+	public TangFuHao() {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	public TangFuHao(int iD, String name, int age) {
+		
+		ID = iD;
+		this.name = name;
+		this.age = age;
+	}
+	public int Sum(int x, int y) {
+		return x+y+2;
+	}
+	public int divide(int x , int y) {
+		return x/y;
+	}
+	
 
 }

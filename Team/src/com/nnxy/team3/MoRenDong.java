235489d545163a0e124e8e9220994c5c45820761
853924164 <853924164@qq.com>
2019-05-21 18:24:11 +0800

    11

--- Team/src/com/nnxy/team3/MoRenDong.java
+++ Team/src/com/nnxy/team3/MoRenDong.java
@@ -2,4 +2,34 @@ package com.nnxy.team3;
 
 public class MoRenDong {
 
+	
+	private int id;
+	private int cds;
+	private int ef;
+	private int asdc;
+	public int getId() {
+		return id;
+	
 }
+	public int getCds() {
+		return cds;
+	}
+	public void setCds(int cds) {
+		this.cds = cds;
+	}
+	public int getEf() {
+		return ef;
+	}
+	public void setEf(int ef) {
+		this.ef = ef;
+	}
+	public int getAsdc() {
+		return asdc;
+	}
+	public void setAsdc(int asdc) {
+		this.asdc = asdc;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	}

f6e365fb873b2742d22510959d07d29acc0528b8
huangbangyi2019 <48992563+huangbangyi2019@users.noreply.github.com>
2019-05-21 17:13:51 +0800

    Update Tangjialian1.java

--- Team/src/com/nnxy/team4/Tangjialian1.java
+++ Team/src/com/nnxy/team4/Tangjialian1.java
@@ -1,5 +1,40 @@
 package com.nnxy.team4;
 
 public class Tangjialian1 {
-
+	private int id;
+	private String name;
+	private String password;
+	private int sex;
+	private int age;
+	public int getId() {
+		return id+10 ;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }

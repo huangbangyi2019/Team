82352452df69a7e713e098c26df8100b83c69dc4
huangbangyi2019 <1094476088@qq.com>
2019-05-21 18:10:50 +0800

    第一次提交..............

--- Team/src/com/nnxy/team1/chenmingchun.java
+++ Team/src/com/nnxy/team1/chenmingchun.java
@@ -7,7 +7,7 @@ public class chenmingchun {
 	private int sex;
 	private  int age;
 	public int getId() {
-		return id + 555555;
+		return id + 555;
 	}
 	public void setId(int id) {
 		this.id = id;

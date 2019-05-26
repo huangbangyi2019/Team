96fb174aeedfe6c125f46f6de581f3d013b87f1e
huangbangyi2019 <48992563+huangbangyi2019@users.noreply.github.com>
2019-05-26 16:29:11 +0800

    Update linrehui.java

--- Team/src/com/nnxy/team2/linrehui.java
+++ Team/src/com/nnxy/team2/linrehui.java
@@ -1,8 +1,16 @@
 package com.nnxy.team2;
 
 public class linrehui {
-  
+	
+	private int id;
+
+	public int getId() {
+		return id + 1000;
 	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+}
 
 	

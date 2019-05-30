6b83cd04c50070711f3f23fd65bc5dcb8d304973
XG402 <XG402@405-75>
2019-05-30 08:56:17 +0800

    20160217034刘华斌

---
+++ Team/src/com/nnxy/team3/Liuhuabin.java
@@ -0,0 +1,22 @@
+package com.nnxy.team3;
+
+public class Liuhuabin {
+	private String name="Αυ»ͺ±σ";
+	private String id="20160217034";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+			
+	public void name() {
+		System.out.println(id+name);
+	}
+}

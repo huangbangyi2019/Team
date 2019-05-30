5388ad1cccf636b186e2affeb16b1067988cd215
huangbangyi2019 <1094476088@qq.com>
2019-05-30 08:45:34 +0800

    新增加一个student类

---
+++ Team/src/com/nnxy/team1/Student.java
@@ -0,0 +1,34 @@
+package com.nnxy.team1;
+
+public class Student {
+
+	private int id;
+	private int age;
+	
+	
+	public void addStudent() {
+		
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}

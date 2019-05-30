f3afd12284087269ef9698304f1304eff37236b5
huangbangyi2019 <1094476088@qq.com>
2019-05-30 09:11:55 +0800

    新增一个Wangkun类

---
+++ Team/src/com/nnxy/team1/Wangkun.java
@@ -0,0 +1,19 @@
+package com.nnxy.team1;
+
+public class Wangkun {
+	private int id;
+	private int name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getName() {
+		return name;
+	}
+	public void setName(int name) {
+		this.name = name;
+	}
+
+}

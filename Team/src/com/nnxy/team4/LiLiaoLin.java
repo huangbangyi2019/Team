7e22fd6e7e490841f058c2ffe79eadebe8c43ca0
huangbangyi2019 <1094476088@qq.com>
2019-05-31 20:28:47 +0800

    eclipse第一次团队开发

---
+++ Team/src/com/nnxy/team4/LiLiaoLin.java
@@ -0,0 +1,17 @@
+package com.nnxy.team4;
+
+public class LiLiaoLin {
+	   String name;
+	   static int age;
+	   public static void main(String[] args) {
+		   if(age != 0) {
+		    for(int i=0; i<age; i++) {
+		     System.out.print("Thanks your coming");
+		     if(age>0) {
+		      System.out.print("intput error");
+		               }
+		                             }
+		                }
+		                                      }
+		                 }
+

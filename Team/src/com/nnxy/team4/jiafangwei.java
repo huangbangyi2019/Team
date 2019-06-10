10d60363b5b995f592d54dc23424dfe8e733f4a3
huangbangyi2019 <1094476088@qq.com>
2019-06-10 12:44:37 +0800

    第一次提交文件

---
+++ Team/src/com/nnxy/team4/jiafangwei.java
@@ -0,0 +1,33 @@
+package com.nnxy.team4;
+
+public class jiafangwei {
+	private int id;
+	public int getId(){
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "jiafangwei [id=" + id + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		jiafangwei other = (jiafangwei) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
